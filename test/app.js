45d5949038a1cba57eaf62ff45180bf4a095ecdf
Vietor Liu <vietor.liu@gmail.com>
2015-05-21 17:41:23 +0800

    Transport more arguments for listen to builtin http(s)

--- test/app.js
+++ test/app.js
@@ -1,9 +1,13 @@
 var bigrest = require("../index");
 
-bigrest.listen(8080, {
+var port = 8080;
+
+bigrest.listen(port, {
     debug: true,
     basepath: __dirname,
     rootwork: function(req, res) {
         res.send("It working...");
     }
+}, function() {
+    console.log('Running on ' + port + ' ...');
 });
