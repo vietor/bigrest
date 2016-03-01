e2e7b643c9cfc76729ef0acd7371144653aab775
Vietor Liu <vietor.liu@gmail.com>
2016-03-01 10:36:11 +0800

    Add more code for demo

--- demo/server.js
+++ demo/server.js
@@ -1,7 +1,12 @@
 'use strict';
 
+var path = require('path');
 var bigrest = require('../index');
 
 var http = bigrest.listen(8080, {
-    basepath: __dirname
+    basepath: __dirname,
+    static: {
+        urlpath: '/files',
+        filepath: path.join(__dirname, 'files')
+    }
 });
